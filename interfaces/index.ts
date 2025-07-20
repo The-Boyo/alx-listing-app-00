6a6d0780166bbb5feb3a67dbdebda82353be73f3
Eliud Metto <eliudmett093@gmail.com>
2025-07-20 23:13:25 +0300

    New layout styles and a new interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,3 +1,18 @@
-interface CardProps {}
-
-interface ButtonProps {}
+export interface PropertyProps {
+    name: string,
+    address: {
+      state: string,
+      city: string,
+      country: string
+    },
+    rating: number,
+    category: Array<string>,
+    price: number,
+    offers: {
+      bed: string,
+      shower: string,
+      occupants: string
+    },
+    image: string,
+    discount?:string
+}
